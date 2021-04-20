2cceb43ca0d58aad006af8c922948ac20c439d3e
Mats haby <mats.haby@live.com>
2021-04-20 21:02:31 +0200

    cleaned code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,34 @@
 const form = document.getElementById('form');
 const error = document.getElementById('error');
 const app = document.getElementById('app');
-
-// buttons
 const btnLogin = document.getElementById('login');
 const btnRegister = document.getElementById('register');
 
+btnLogin.addEventListener('click', loginUser);
+btnRegister.addEventListener('click', createUser);
+
+/**
+ * Calls a fetch function to post a login request
+ * with login as argument to the URL
+ */
 function loginUser() {
     fetchPostToBackend('login');
 }
 
-btnLogin.addEventListener('click', loginUser);
-
+/**
+ * Calls a fetch function to post a login request
+ * with register as argument to the URL
+ */
 function createUser() {
     fetchPostToBackend('register');
 }
-btnRegister.addEventListener('click', createUser);
 
+/**
+ * Sends a fetch with post method to backend to either login or register a user
+ * when authenticated login the user and save their token into local storage.
+ *
+ * @param {string} type
+ */
 function fetchPostToBackend(type) {
     const adminUser = {
         email: document.getElementById('email').value,
@@ -34,7 +46,6 @@ function fetchPostToBackend(type) {
         .then((res) => res.json())
         .then((data) => {
             if (data.error) throw new Error(data.error);
-            console.log(data);
             authenticated(data);
             saveLoginSession(data.token);
         })
@@ -47,7 +58,11 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
-// what to do when authenticated
+/**
+ * Generates the login-page for the user
+ *
+ * @param {object} data
+ */
 function authenticated(data) {
     app.innerHTML = `
     <div class="" id="prem">
@@ -58,35 +73,42 @@ function authenticated(data) {
                 <input type="checkbox" id="checkbox" ${checkbox(
                     data.subscription
                 )}>
-
                 <div class="b-input"></div>
-             </label>
-                             <button class="btn" id="save">Save</button>
-                <button class="btn" id="logout">Logout</button>
-                <div id="mm" class="mm"></div>
-    </div>
-
-
-    </div>
-    `;
+            </label>
+            <button class="btn" id="save">Save</button>
+            <button class="btn" id="logout">Logout</button>
+            <div id="mm" class="mm"></div>
+        </div>
+    </div>`;
 
     document.getElementById('save').addEventListener('click', () => {
         const checkboxValue = document.getElementById('checkbox').checked;
-
         updateSubscription(data.token, { subscription: checkboxValue });
     });
 
     document.getElementById('logout').addEventListener('click', logOut);
 }
-
+/**
+ * Returs the string checked if the value of subscription is true on the user
+ * to make the checkbox checked
+ *
+ * @param {boolean} subscriber
+ * @returns {string}
+ */
 function checkbox(subscriber) {
     if (subscriber) {
         return 'checked';
     }
 }
 
+/**
+ * Updates the subscription value on the user when the button is clicked.
+ * Checked checkbox = subscriber
+ *
+ * @param {string} token
+ * @param {boolean} subscribe
+ */
 function updateSubscription(token, subscribe) {
-    console.log('toek!', token);
     fetch(`https://fed20d-dw-nl-backend.herokuapp.com/user/${token}`, {
         method: 'PATCH',
         headers: {
@@ -107,10 +129,20 @@ function updateSubscription(token, subscribe) {
         });
 }
 
+/**
+ * Saves the token into the browsers local storage.
+ *
+ * @param {string} token
+ */
 function saveLoginSession(token) {
     localStorage.setItem('token', token);
 }
 
+/**
+ * Checks if the token is present in the local storage.
+ * If the token is there, then login the user directly.
+ *
+ */
 function isLoggedIn() {
     const token = localStorage.getItem('token');
     if (token) {
@@ -127,6 +159,11 @@ function isLoggedIn() {
     }
 }
 
+/**
+ * Logout out the user, remove the token from local storage and reload the package
+ * Redirects the user back to the loginpage.
+ *
+ */
 function logOut() {
     localStorage.removeItem('token');
     location.reload();
